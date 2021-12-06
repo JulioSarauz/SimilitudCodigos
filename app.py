a2bcf705f8c99aff8848fd352ef09dcbbe72b9ce
Julio Sarauz <juliosarauz1i@gmail.com>
2021-12-06 17:10:27 -0500

    primer

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import procesos 
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask (__name__, template_folder='template') 
+app.config['UPLOAD_FOLDER'] = "data"
+nombre = "SIMILITUD DE CODIGOS"
+detalle = "Para los programadores que a menudo escriben código y cambian el código, si el código escrito antes necesita cambiarse después de mucho tiempo, es bastante difícil encontrar la diferencia."
+
+
+
+#INICIO DE LA PAGINA WEB
+@app.route ('/', methods=['GET','POST']) 
+def home (): 
+   contenido = procesos.buscarArchivos()
+   return render_template ('index.html',nombre=nombre, detalle=detalle,contenido=contenido) 
+
+@app.route ('/prueba', methods=['GET','POST']) 
+def prueba(): 
+   contenido = procesos.buscarArchivos()
+   nombres = request.args['nombres']
+   coseno = procesos.comparar(nombres)
+   jacard = procesos.compararJacard(nombres)
+   return render_template ('index.html',nombre=nombre, detalle=detalle,contenido=contenido, coseno=coseno, jacard=jacard) 
+
+
+@app.route('/upload', methods=['POST'])
+def uploadFile():
+    if request.method == "POST":
+        contenido = procesos.buscarArchivos()
+        f = request.files['archivoRecibido']
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+        return render_template ('index.html',nombre=nombre, detalle=detalle,contenido=contenido) 
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ procesos.py
@@ -0,0 +1,79 @@
+import os
+from os import remove
+import numpy as np
+import re, math
+from collections import Counter
+
+
+Word = re.compile(r'\w+')
+def get_cosine(vec1, vec2):
+     intersection = set(vec1.keys()) & set(vec2.keys())
+     numerator = sum([vec1[x] * vec2[x] for x in intersection])
+     sum1 = sum([vec1[x]**2 for x in vec1.keys()])
+     sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+     denominator = math.sqrt(sum1) * math.sqrt(sum2)
+     if not denominator:
+        return 0.0
+     else:
+        return float(numerator) / denominator
+
+def text_to_vector(text):
+     words = Word.findall(text)
+     return Counter(words)
+
+
+
+def distanciaCoseno(text1, text2):
+    vector1 = text_to_vector(text1)
+    vector2 = text_to_vector(text2)
+    cosine = get_cosine(vector1, vector2)
+    return round(cosine,2)
+
+def get_jaccard_sim(str1, str2): 
+    a = set(str1.split()) 
+    b = set(str2.split())
+    c = a.intersection(b)
+    return round(float(len(c)) / (len(a) + len(b) - len(c)),2)
+
+def generarArchivo(name,texto):  
+    file = open("data/"+str(name)+".txt", "w")   
+    file.write(texto)
+    file.close()
+
+
+def eliminarArchivo(nombre):
+    remove('data/'+str(nombre))
+    
+def buscarArchivos():
+    contenido = os.listdir('data')
+    return contenido
+
+def leerArchivo(name):
+    contenido = []
+    with open("data/"+str(name),"r") as archivo:
+        for linea in archivo:
+            contenido.append(linea)
+    return contenido
+
+def comparar(archivos):
+    matriz = []
+    archivos = archivos.split(',')
+    for i in range(0,len(archivos)):
+        respuesta=[]
+        for j in range(0,len(archivos)):
+            respuesta.append(distanciaCoseno(str(leerArchivo(archivos[i])),str(leerArchivo(archivos[j]))))
+        matriz.append(respuesta)
+    return matriz
+
+def compararJacard(archivos):
+    matriz = []
+    archivos = archivos.split(',')
+    for i in range(0,len(archivos)):
+        respuesta=[]
+        for j in range(0,len(archivos)):
+            respuesta.append(get_jaccard_sim(str(leerArchivo(archivos[i])),str(leerArchivo(archivos[j]))))
+        matriz.append(respuesta)
+    return matriz
+   
+
+
